b495deb603774f4b7822bacf7e211cff87c7130b
Shubham Thorat <shubhamthorat230520011@gmail.com>
2026-08-30 10:54:40 +0530

    state management riverpord

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:relay/app/app.dart';
 
 void main() {
-  runApp(const RelayApp());
+  runApp(
+    const ProviderScope(
+      child: RelayApp(),
+    ),
+  );
 }
