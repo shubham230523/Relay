136b3151125a778919ddba2eb9f90ba3bae94de0
Shubham Thorat <shubhamthorat230520011@gmail.com>
2026-08-30 10:47:04 +0530

    app entry widget

--- lib/app/relay_app.dart
+++ lib/app/relay_app.dart
@@ -7,6 +7,7 @@ class RelayApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Relay',
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Center(
           child: Text('Relay'),
