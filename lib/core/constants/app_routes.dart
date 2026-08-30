3a29065bd79ace4156f62974e3321a048e352195
Shubham Thorat <shubhamthorat230520011@gmail.com>
2026-08-30 13:11:34 +0530

    route constants created

---
+++ lib/core/constants/app_routes.dart
@@ -0,0 +1,16 @@
+class AppRoutes {
+  AppRoutes._();
+
+  static const String dashboard = '/';
+  
+  static const String automations = '/automations';
+  static const String createAutomation = '/automations/create';
+  static const String workflowDetails = '/automations/details';
+  
+  static const String executions = '/executions';
+  static const String executionDetails = '/executions/details';
+  
+  static const String templates = '/templates';
+  static const String integrations = '/integrations';
+  static const String settings = '/settings';
+}
--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -1,1 +1,2 @@
 export 'app_layout.dart';
+export 'app_routes.dart';
