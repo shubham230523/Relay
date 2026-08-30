8d4c254db4600aff4de9a572c4cb5193c66ee935
Shubham Thorat <shubhamthorat230520011@gmail.com>
2026-08-30 12:18:03 +0530

    responsive page indicator created

---
+++ lib/core/widgets/page_container.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import '../constants/constants.dart';
+import '../utils/utils.dart';
+
+class PageContainer extends StatelessWidget {
+  final Widget child;
+  final double? maxWidth;
+  final bool isScrollable;
+  final EdgeInsetsGeometry? padding;
+  final ScrollController? scrollController;
+  final AlignmentGeometry alignment;
+
+  const PageContainer({
+    super.key,
+    required this.child,
+    this.maxWidth = 1200,
+    this.isScrollable = true,
+    this.padding,
+    this.scrollController,
+    this.alignment = Alignment.topCenter,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final horizontalPadding = AppBreakpoints.isMobile(context)
+        ? AppLayout.screenPadding
+        : AppBreakpoints.isTablet(context)
+            ? AppLayout.spaceL
+            : AppLayout.spaceXL;
+
+    final effectivePadding = padding ??
+        EdgeInsets.symmetric(
+          horizontal: horizontalPadding,
+          vertical: AppLayout.spaceM,
+        );
+
+    Widget content = Padding(
+      padding: effectivePadding,
+      child: child,
+    );
+
+    if (maxWidth != null) {
+      content = Align(
+        alignment: alignment,
+        child: ConstrainedBox(
+          constraints: BoxConstraints(maxWidth: maxWidth!),
+          child: content,
+        ),
+      );
+    }
+
+    if (isScrollable) {
+      return SingleChildScrollView(
+        controller: scrollController,
+        physics: const AlwaysScrollableScrollPhysics(),
+        child: content,
+      );
+    }
+
+    return content;
+  }
+}
--- lib/core/widgets/widgets.dart
+++ lib/core/widgets/widgets.dart
@@ -1,1 +1,2 @@
 export 'responsive_builder.dart';
+export 'page_container.dart';
